fc876a6fe6260b5c88125512563c0a03cb910b9d
Rahul Mohan <mohanrahul93@gmail.com>
2022-02-28 21:58:21 +0530

    Done

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
-
+const Shape = require("./shape");
 // declare class
+class Circle extends Shape {
+  constructor(radius, color) {
+    super(color);
+    this.radius = radius;
+  }
 
-
+  calculateArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
 // export class using module.exports
+exports.Circle = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {}
+  calculateArea() {}
+}
 
 // export class using module.exports
+exports.Shape = Shape;
